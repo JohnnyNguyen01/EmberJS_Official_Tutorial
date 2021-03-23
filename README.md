28f280c75948b43aed6542466db9e9557dcb9e42
johnny <johnnynguyenwork@gmail.com>
2021-03-23 17:16:21 +1100

    second progress

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 
 ## Notes
   - Assets such as images are placed in the `public` folder, and are automatically compiled and understood by Ember.
+
+## Part 2 
+Learning how to:
+- Define Routes
+- Customize URLs
+- Link Pages with `<LinkTo>` component
+- Passing arguments and attributes to components
+
+Everytime we generate a new route, we do the following:
+1. Map the route in Router.js
+2. Create a template for the Route. (Templates are the 'view')
+3. [OPTIONAL] Define an explicit path for errors or custom URLs
--- app/router.js
+++ app/router.js
@@ -1,9 +1,20 @@
 import EmberRouter from '@ember/routing/router';
 import config from 'super-rentals/config/environment';
 
+/**
+ * This class manages what pages are available to the Router.
+ */
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+/**
+ * Add a route named `about` which is accessible via URL. i.e. /about
+ * the `path` object for contact route, explicity  creates a custom path we can use
+ */
+Router.map(function () {
+  this.route('about');
+  this.route('contact', { path: '/getting-in-touch' });
+  this.route('error', { path: '/*' });
+});
